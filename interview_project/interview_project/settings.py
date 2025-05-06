d0486ff8c11c2180120920ac4c88d28f99a45a4f
Om Bhandarkar <ombhandarkar1206@gmail.com>
2025-05-06 13:17:29 +0530

    update

--- interview_project/interview_project/settings.py
+++ interview_project/interview_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-86lu12%-ybwnk7xz_m@g=frci-^m+bm0i5bwk7=@s%@kc^z*l3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('RENDER_EXTERNAL_HOSTNAME'), 'interview_project.up.railway.app'
+]
 
 
 # Application definition
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
